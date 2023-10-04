46754af6a8930d2d65a2c36af5f83d72ab1276f4
HangNT <hangnt169@fpt.edu.vn>
2023-10-04 19:12:04 +0700

    Init project

---
+++ src/mob1014_hangnt169_l1/MOB1014_HangNT169_L1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mob1014_hangnt169_l1;
+
+/**
+ *
+ * @author hangnt
+ */
+public class MOB1014_HangNT169_L1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
